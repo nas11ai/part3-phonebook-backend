ae76058609372e61b420f8542ef5c75220725db3
nas11ai <74126181+nas11ai@users.noreply.github.com>
2022-04-25 15:14:45 +0800

    3.13: phonebook database step 1

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 const app = express()
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
 
 app.use(express.json())
 app.use(cors())
@@ -37,7 +17,8 @@ morgan.token('request-body', function getRequestBody(req, res) {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-body'))
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({})
+        .then(persons => response.json(persons))
 })
 
 app.get('/info', (request, response) => {
