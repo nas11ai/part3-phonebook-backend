b8249eb0741539124d8ba1627e12683da188397d
nas11ai <74126181+nas11ai@users.noreply.github.com>
2022-04-26 14:08:46 +0800

    3.17: phonebook database step 5

--- index.js
+++ index.js
@@ -64,6 +64,19 @@ app.post('/api/persons', (request, response) => {
         .then(savedNote => response.json(savedNote))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => response.json(updatedPerson))
+        .catch(error => next(error))
+})
+
 const errorHandler = (error, request, response, next) => {
     console.log(error.message)
 
